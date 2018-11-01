42fc7c0e4feb014d08ab45b84ced3adf1c560220
Sergey <xxsmotur@gmail.com>
2018-11-01 17:34:08 +0500

    hardware: add regexp parsing

--- hardware/hardware.go
+++ hardware/hardware.go
@@ -1,16 +1,17 @@
 package hardware
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
 	"runtime"
+	"strconv"
 )
 
-const (
-	procMem           = "/proc/meminfo"
-	memoryTotalRegexp = regexp.MustCompile(`MemTotal:\s*([0-9]+) kB`)
-)
+const procMem = "/proc/meminfo"
+
+var memoryTotalRegexp = regexp.MustCompile(`MemTotal:\s*([0-9]+) kB`)
 
 // GetNumberOfCores returns number of cores at machine
 func GetNumberOfCores() int {
@@ -24,14 +25,29 @@ func GetHostname() (string, error) {
 
 // GetMemoryCapacity returns total memory from /proc/meminfo.
 func GetMemoryCapacity() (uint64, error) {
-	out, err := ioutil.ReadFile(procMem)
+	f, err := ioutil.ReadFile(procMem)
 	if err != nil {
 		return 0, err
 	}
 
-	memoryCapacity, err := parseCapacity(out, memoryCapacityRegexp)
+	memoryCapacity, err := parse(f, memoryTotalRegexp)
 	if err != nil {
 		return 0, err
 	}
 	return memoryCapacity, err
+}
+
+// parse is a helpful method for parsing of values
+func parse(b []byte, r *regexp.Regexp) (uint64, error) {
+	matches := r.FindSubmatch(b)
+	if len(matches) != 2 {
+		return 0, fmt.Errorf("failed to match regexp in output: %q", string(b))
+	}
+	m, err := strconv.ParseUint(string(matches[1]), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	// Convert to bytes.
+	return m * 1024, err
 }
