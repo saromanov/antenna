45c343321f3270b6eb49dfd82ecc659447f952f8
Sergey <xxsmotur@gmail.com>
2018-11-01 17:44:06 +0500

    Add method ClientContainerConfig

--- structs/v1/container.go
+++ structs/v1/container.go
@@ -2,6 +2,13 @@ package v1
 
 import "time"
 
+// ClientContainerConfig defines representation of the
+// Configuration before starting of container client
+type ClientContainerConfig struct {
+	//CertPathEnv defines path to the cert files
+	CertPathEnv string `json:"cert_path_env"`
+}
+
 // Container define struct for container representation
 type Container struct {
 	CreatedTime time.Time `json:"created_time,omitempty"`
