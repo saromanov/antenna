1082a70970a06b70afa13e6dada89ec2daaba34b
Sergey <xxsmotur@gmail.com>
2018-11-01 17:07:20 +0500

    Get memory capacity

--- hardware/hardware.go
+++ hardware/hardware.go
@@ -1,9 +1,12 @@
 package hardware
 
 import (
+	"io/ioutil"
 	"os"
 	"runtime"
 )
+
+const procMem = "/proc/meminfo"
 
 // GetNumberOfCores returns number of cores at machine
 func GetNumberOfCores() int {
@@ -13,4 +16,18 @@ func GetNumberOfCores() int {
 // GetHostname returns hostname of machine
 func GetHostname() (string, error) {
 	return os.Hostname()
+}
+
+// GetMemoryCapacity returns total memory from /proc/meminfo.
+func GetMemoryCapacity() (uint64, error) {
+	out, err := ioutil.ReadFile(procMem)
+	if err != nil {
+		return 0, err
+	}
+
+	memoryCapacity, err := parseCapacity(out, memoryCapacityRegexp)
+	if err != nil {
+		return 0, err
+	}
+	return memoryCapacity, err
 }
