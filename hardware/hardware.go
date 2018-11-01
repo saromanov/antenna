5aed3fc390280b9fc43f95e70593c12e8682e09a
Sergey <xxsmotur@gmail.com>
2018-11-01 16:49:18 +0500

    Add GetHostname method

--- hardware/hardware.go
+++ hardware/hardware.go
@@ -1,8 +1,16 @@
 package hardware
 
-import "runtime"
+import (
+	"os"
+	"runtime"
+)
 
 // GetNumberOfCores returns number of cores at machine
 func GetNumberOfCores() int {
 	return runtime.NumCPU()
+}
+
+// GetHostname returns hostname of machine
+func GetHostname() (string, error) {
+	return os.Hostname()
 }
