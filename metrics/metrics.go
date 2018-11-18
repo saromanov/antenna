5dc3b46224fdf9784d0fc9cce8ba63738dda3e17
Sergey <xxsmotur@gmail.com>
2018-11-18 17:43:36 +0500

    Add draft for new metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -21,3 +21,38 @@ type PrometheusCollector struct {
 	errors           prometheus.Gauge
 	containerMetrics []containerMetric
 }
+
+func NewPrometheusCollector(i infoProvider, f ContainerLabelsFunc, includedMetrics container.MetricSet) *PrometheusCollector {
+	if f == nil {
+		f = DefaultContainerLabels
+	}
+	c := &PrometheusCollector{
+		infoProvider:        i,
+		containerLabelsFunc: f,
+		errors: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "container",
+			Name:      "scrape_error",
+			Help:      "1 if there was an error while getting container metrics, 0 otherwise",
+		}),
+		containerMetrics: []containerMetric{
+			{
+				name:      "container_last_seen",
+				help:      "Last time a container was seen by the exporter",
+				valueType: prometheus.GaugeValue,
+				getValues: func(s *info.ContainerStats) metricValues {
+					return metricValues{{value: float64(time.Now().Unix())}}
+				},
+			},
+		},
+		includedMetrics: includedMetrics,
+	}
+		c.containerMetrics = append(c.containerMetrics, []containerMetric{
+			{
+				name:      "container_cpu_user_seconds_total",
+				help:      "Cumulative user cpu time consumed in seconds.",
+				valueType: prometheus.CounterValue,
+			}
+		}
+}
+			
+
