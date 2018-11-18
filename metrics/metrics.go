8b4c6d3907334a95468482e1eae3581ead37f6c3
Sergey <xxsmotur@gmail.com>
2018-11-18 17:41:47 +0500

    init for container metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,1 +1,23 @@
 package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type metricValue struct {
+	value  float64
+	labels []string
+}
+
+type containerMetric struct {
+	name        string
+	help        string
+	valueType   prometheus.ValueType
+	extraLabels []string
+}
+
+type PrometheusCollector struct {
+	infoProvider     infoProvider
+	errors           prometheus.Gauge
+	containerMetrics []containerMetric
+}
