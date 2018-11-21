2acaf16e6d4b26c931407b2e5b220d5928b00178
Sergey <xxsmotur@gmail.com>
2018-11-21 16:57:17 +0500

    Add New method to the Storage config

--- storage/storage.go
+++ storage/storage.go
@@ -5,6 +5,7 @@ import structs "github.com/saromanov/antenna/structs/v1"
 // Storage defines main interface for Providing
 // handling of storage data
 type Storage interface {
+	New(*Config) (Storage, error)
 	Add(*structs.ContainerStat) error
 	Close() error
 	Search() ([]*structs.ContainerStat, error)
