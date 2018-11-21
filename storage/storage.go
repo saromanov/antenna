70902e656b2cd6783b5c99d03e4f57d2e4b604d2
Sergey <xxsmotur@gmail.com>
2018-11-21 13:39:35 +0500

    Add New method for init Storage

--- storage/storage.go
+++ storage/storage.go
@@ -7,3 +7,10 @@ type Storage interface {
 	Close() error
 	Search() ([]*structs.ContainerStat, error)
 }
+
+// New creates storage based on name
+// At the init stage, its supports only InfluxDB
+// So, "name" attribute is unused at this moment
+func New(name string) (Storage, error) {
+	return nil, nil
+}
