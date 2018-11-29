62de6c0e6420b7ecdb44adcc1bbecaa381f6cd2f
Sergey <xxsmotur@gmail.com>
2018-11-29 23:37:19 +0500

    Rewriting of influxdb module

--- storage/influxdb/influxdb.go
+++ storage/influxdb/influxdb.go
@@ -2,61 +2,59 @@ package influxdb
 
 import (
 	"sync"
-	"fmt"
-	influxdb "github.com/influxdata/influxdb/client/v2"
-	"github.com/saromanov/antenna/structs/v1"
+
+	"github.com/influxdata/influxdb/client/v2"
 	"github.com/saromanov/antenna/storage"
+	structs "github.com/saromanov/antenna/structs/v1"
 )
 
 type influxDB struct {
-	client   *influxdb.Client
+	client   client.Client
 	database string
 	lock     sync.Mutex
 }
 
 // New creates storage based on name
 // At the init stage, its supports only InfluxDB
-func New(conf *Config) (Storage, error) {
-	return new(conf.URL)
+func New(conf *storage.Config) (storage.Storage, error) {
+	return new(conf)
 }
 
-
-
-func new(url string) (storage.Storage, error) {
-	config := &influxdb.Config{
-		URL:      url,
-		Username: username,
-		Password: password,
+func new(conf *storage.Config) (storage.Storage, error) {
+	config := client.HTTPConfig{
+		Addr:     conf.URL,
+		Username: conf.Username,
+		Password: conf.Password,
 	}
-	client, err := influxdb.NewClient(*config)
+	cli, err := client.NewHTTPClient(config)
 	if err != nil {
 		return nil, err
 	}
 	return &influxDB{
-		client: client,
+		client: cli,
 	}, nil
 }
 
 // Add provides adding of stat
-func (i*influxDB) Add(metrics *structs.ContainerStat) error {
+func (i *influxDB) Add(metrics *structs.ContainerStat) error {
 	i.lock.Lock()
 	defer i.lock.Unlock()
-	var points []*influxdb.Point
-	if err := c.Write(points); err != nil {
+	var points []*client.Point
+	if err := i.client.Write(points); err != nil {
 		return err
 	}
-	if err := c.Close(); err != nil {
-    	return err
+	if err := i.client.Close(); err != nil {
+		return err
 	}
 	return nil
 }
 
-func (i*influxDB) toPoints(metrics *structs.ContainerStat)[]*influxdb.Point {
+func (i *influxDB) toPoints(metrics *structs.ContainerStat) []*client.Point {
 	if len(metrics) == 0 {
 		return nil
 	}
-	points := []*influxdb.Point{}
-	points = append(points, makePoint("cpu", metrics.CPU)
+	points := []*client.Point{}
+	points = append(points, makePoint("cpu", metrics.CPU))
 	return nil
 }
 
