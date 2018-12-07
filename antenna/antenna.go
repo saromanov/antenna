9611a630f1181655eb716982fd91fe520426bdf4
Sergey <xxsmotur@gmail.com>
2018-12-07 21:47:38 +0500

    antenna: Add comment

--- antenna/antenna.go
+++ antenna/antenna.go
@@ -2,37 +2,41 @@ package antenna
 
 import (
 	"net/http"
+
 	"github.com/saromanov/antenna/storage"
 )
+
 // Application provides definition of the main
 // interface for app
 type Application struct {
 	httpClient http.Client
-	Store storage.Storage
-	events chan *ContainerEvent
+	Store      storage.Storage
+	events     chan *ContainerEvent
 }
 
+// Container event defines events on containers
 type ContainerEvent struct {
 }
 
 // Start provides starting of the app
-func (a*Application) Start() error {
+func (a *Application) Start() error {
 	return nil
 }
 
-func (a*Application) startEventWatcher(){
+func (a *Application) startEventWatcher() {
 	select {
-	case event := <-self.events:
-		
+	case event := <-a.events:
+
 	}
 }
+
 type antenna struct {
-	store storage.Storage
+	store      storage.Storage
 	httpClient *http.Client
 }
 
 // New provides initialization on the app
-func New()(Application, error){
+func New() (Application, error) {
 	return Application{
 		store: nil,
 	}, nil
