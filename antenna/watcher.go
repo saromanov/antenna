670952c581e2889d064fb013740a96796830f354
Sergey <xxsmotur@gmail.com>
2018-12-11 23:20:17 +0500

    Add watcher for get list of containers

---
+++ antenna/watcher.go
@@ -0,0 +1,20 @@
+package antenna
+
+import (
+	"fmt"
+	"github.com/saromanov/antenna/container/docker"
+)
+// watcher creates object for watch running containers
+// and gettign info from this
+type watcher struct {
+	dockerClient *docker.Docker
+}
+
+func (w*watcher) Watch() {
+	containers, err := w.dockerClient.GetContainers()
+	if err != nil {
+		fmt.Printf("unable to get list of containers: %v\n", err)
+	}
+
+	fmt.Println("Containers: ", containers)
+}
