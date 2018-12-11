f875bf00f9178b8a15b90324b1964b61bf61f47c
Sergey <xxsmotur@gmail.com>
2018-12-11 23:27:16 +0500

    structs: Add ListContainersOption for search containers

--- structs/v1/container.go
+++ structs/v1/container.go
@@ -1,6 +1,9 @@
 package v1
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 // ClientContainerConfig defines representation of the
 // Configuration before starting of container client
@@ -8,6 +11,18 @@ type ClientContainerConfig struct {
 	//CertPathEnv defines path to the cert files
 	CertPathEnv string `json:"cert_path_env"`
 	Endpoint    string `json:"endpoint"`
+}
+
+// ListContainersOptions provides options for searching containers
+// copyed from docker client definition
+type ListContainersOptions struct {
+	All     bool
+	Size    bool
+	Limit   int
+	Since   string
+	Before  string
+	Filters map[string][]string
+	Context context.Context
 }
 
 // Container define struct for container representation
