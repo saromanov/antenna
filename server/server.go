76a6db03e4a4e614e983b08e0bb17d00a81345c2
Sergey <xxsmotur@gmail.com>
2019-07-03 21:10:59 +0500

    Draft for api for getting affregated results

--- server/server.go
+++ server/server.go
@@ -1,16 +1,34 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/saromanov/antenna/storage"
+	structs "github.com/saromanov/antenna/structs/v1"
 )
 
-func AggregateMetrics(w http.ResponseWriter, r *http.Request) {
-
+// server defines api endpoints
+type server struct {
+	st storage.Storage
 }
 
-func Start(address string) {
-	http.HandleFunc("/aggregate", AggregateMetrics)
+// AggregateMetrics defines api endpoint for getting aggregated
+// metrics from container
+func (s *server) AggregateMetrics(w http.ResponseWriter, r *http.Request) {
+	response, err := s.st.Aggregate(&structs.AggregateSearchRequest{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(response)
+}
+
+func Start(st storage.Storage, address string) {
+
+	s := &server{st: st}
+	http.HandleFunc("/aggregate", s.AggregateMetrics)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
