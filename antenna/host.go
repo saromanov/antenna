1e5851cf86e75211b2a336ba9080958dad2086b1
Sergey <xxsmotur@gmail.com>
2019-07-30 09:04:03 +0500

    Getting hostname

--- antenna/host.go
+++ antenna/host.go
@@ -4,6 +4,7 @@ import "github.com/saromanov/antenna/hardware"
 
 type HostInfo struct {
 	Cores int
+	Hostname string
 }
 
 // hostWatcher provides checking of the host machine
@@ -12,5 +13,10 @@ func getStaticHostInfo() (*HostInfo, error) {
 	host := &HostInfo{}
 	cores := hardware.GetNumberOfCores()
 	host.Cores = cores
+	hn, err := hardware.GetHostname()
+	if err != nil {
+		return nil, err
+	}
+	host.Hostname = hn
 	return host, nil
 }
