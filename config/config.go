39060ae35295bf1628351bb2d3fc952ad925e720
Sergey <xxsmotur@gmail.com>
2019-08-01 07:53:59 +0500

    update

--- config/config.go
+++ config/config.go
@@ -3,5 +3,5 @@ package config
 // Config defines configuration for the antenna
 type Config struct {
 	HTTPAddress   string `yaml:"http_address"`
-	InfluxAddress string `yaml"influx_addrss"`
+	InfluxAddress string `yaml:"influx_addrss"`
 }
