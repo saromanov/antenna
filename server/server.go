654f3effaa50029f98255bf552a052d10f983006
Sergey <xxsmotur@gmail.com>
2019-12-22 16:58:11 +0500

    filling of the method for info

--- server/server.go
+++ server/server.go
@@ -42,14 +42,20 @@ func (s *server) AggregateMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) Info(w http.ResponseWriter, r *http.Request) {
+	response := s.st.Info()
+	result, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
 }
 
 func Start(st storage.Storage, address string) {
-
 	s := &server{st: st}
 	http.HandleFunc("/v1/aggregate", s.AggregateMetrics)
-	http.HandleFunc("/v1.info")
+	http.HandleFunc("/v1.info", s.Info)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
