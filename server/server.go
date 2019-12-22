5a3a572513bc286cbe4d3527963ba2a10d512285
Sergey <xxsmotur@gmail.com>
2019-12-22 22:30:07 +0500

    fix typo

--- server/server.go
+++ server/server.go
@@ -55,7 +55,7 @@ func (s *server) Info(w http.ResponseWriter, r *http.Request) {
 func Start(st storage.Storage, address string) {
 	s := &server{st: st}
 	http.HandleFunc("/v1/aggregate", s.AggregateMetrics)
-	http.HandleFunc("/v1.info", s.Info)
+	http.HandleFunc("/v1/info", s.Info)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
