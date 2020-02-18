2eb06286b1bd275a0c8829adedb797325d035cd6
Sergey <xxsmotur@gmail.com>
2020-02-18 10:52:30 +0500

    Draft for sending message to the slack

--- notifications/slack/slack.go
+++ notifications/slack/slack.go
@@ -1,22 +1,32 @@
 package slack
 
 import (
+	"fmt"
+
 	"github.com/nlopes/slack"
 	"github.com/saromanov/antenna/notifications"
 	structs "github.com/saromanov/antenna/structs/v1"
 )
+
 type Slack struct {
-	api *slack.Api
+	api       *slack.Client
+	channelID string
 }
 
 // New provides initialization of the Slack client
-func New(token string) notifications.Notification {
-	return &Slack {
-		api: slack.Api(token),
+func New(token, channelID string) notifications.Notifications {
+	return &Slack{
+		api:       slack.New(token),
+		channelID: channelID,
 	}
 }
 
 // Send provides sending of the message
-func (s*Slack) Send(n *structs.Notification) error {
+func (s *Slack) Send(n *structs.Notification) error {
+	channelID, timestamp, err := s.api.PostMessage("CHANNEL_ID", slack.MsgOptionText(n.Body, false), nil)
+	if err != nil {
+		return fmt.Errorf("unable to post message: %v", err)
+	}
+	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 	return nil
 }
